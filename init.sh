c6ac21fa775c4835bb6d0fb9a7f2f1a2b24002d3
Joschka Lingemann <joschka.lingemann@cern.ch>
2016-01-12 10:50:57 +0100

    Updating dependencies to the latest used in FCCSW

--- init.sh
+++ init.sh
@@ -8,11 +8,11 @@ if [[ "$unamestr" == 'Linux' ]]; then
     platform='Linux'
     if [[ -d /afs/cern.ch/sw/lcg ]] && [[ `dnsdomainname` = 'cern.ch' ]] ; then
 	#should check domain to make sure we're at CERN
-	#or is this software available somewhere in Lyon? 
+	#or is this software available somewhere in Lyon?
 	sw_afs=1
         export PATH=/afs/cern.ch/sw/lcg/contrib/CMake/2.8.9/Linux-i386/bin:${PATH}
-	source /afs/cern.ch/sw/lcg/contrib/gcc/4.8.1/x86_64-slc6/setup.sh
-	source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.20/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh
+	source /afs/cern.ch/sw/lcg/contrib/gcc/4.9.3/x86_64-slc6/setup.sh
+	source /afs/cern.ch/sw/lcg/releases/ROOT/6.04.06-458fa/x86_64-slc6-gcc49-opt/bin/thisroot.sh
 	echo cmake and root taken from /afs/cern.ch/sw/lcg
     fi
     export LD_LIBRARY_PATH=$ALBERS/lib:$LD_LIBRARY_PATH
